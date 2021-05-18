f7ce3d731b4d5e3ed4028c9942a451b449b02d96
QuantumedBox <hectorgreat19@gmail.com>
2021-05-18 07:26:32 +0500

    working loader + extern

---
+++ scripts/openRogue/extern.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import importlib
+import subprocess
+
+
+def depend(*args):
+	"""
+	Resolves pip dependencies on runtime
+	"""
+
+	try:
+		import pip
+	except ImportError:
+		# pip is ensured to be, but you have to call ensurepip for it to be detectable
+		if os.path.basename(sys.executable).split('.')[0] == "pypy3":
+			subprocess.check_call([sys.executable, "-m", "ensurepip"])
+		else:
+			print("pip isn't present, trying to get it from get-pip.py")
+			subprocess.check_call([sys.executable, "get-pip.py"])
+
+	for module in args:
+		try:
+			importlib.import_module(module)
+			print("module {} in present".format(module))
+		except ImportError:
+			print("cannot find {}, installing it from pip...".format(module))
+			subprocess.check_call([sys.executable, "-m", "pip", "install", module])
